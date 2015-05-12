7e3bcf9e16c2241da3989f30210813131cb22c99
nmatta <kumar_nav2000@yahoo.com>
2015-05-12 23:08:29 +0530

    Made few changes to routing logic and UI

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+requirejs.config({
+
+});
+
+
+require(['jQuery','bootstrap'], function ($, bs) {
+
+    var app = {
+        initialize: function () {
+            // Your code here
+        }
+    };
+
+    app.initialize();
+});
